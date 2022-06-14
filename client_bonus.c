b470a90b9a2fddccc4ba6f7e042faacc29946e29
sumnii <sumsong@student.42seoul.kr>
2022-06-14 13:43:03 +0900

    [FIX] now they can communicate with huge string.

---
+++ client_bonus.c
@@ -0,0 +1,75 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "minitalk.h"
+
+void	ft_send_str(pid_t pid, char *message);
+void	ft_send_signal(pid_t pid, char chr);
+void	ft_write_sig(int signum);
+
+int main(int argc, char **argv)
+{
+	int		pid;
+	char	*message;
+
+	if (argc != 3)
+	{
+		printf("type => \"./client <pid> <string>\"\n");
+		exit (1);
+	}
+	signal(SIGUSR1, (void *)ft_write_sig);
+	pid = ft_atoi(argv[1]);
+	message = argv[2];
+	ft_send_str(pid, message);
+	while (1)
+		pause();
+}
+
+void	ft_send_str(pid_t pid, char *message)
+{
+	while (*message)
+	{
+		ft_send_signal(pid, *message);
+		usleep(40);
+		++message;
+	}
+	ft_send_signal(pid, '\0');
+}
+
+void	ft_send_signal(pid_t pid, char chr)
+{
+	char			cnt;
+	unsigned char	bit;
+
+	cnt = 0;
+	bit = 128;
+	while (++cnt <= 8)
+	{
+		if ((chr & bit) == bit)
+		{
+			if (kill(pid, SIGUSR1) == -1)
+				exit(1);
+		}
+		else
+		{
+			if (kill(pid, SIGUSR2) == -1)
+				exit(1);
+		}
+		usleep(50);
+		bit >>= 1;
+	}
+}
+
+void	ft_write_sig(int signum)
+{
+	if (signum == SIGUSR1)
+	{
+		write(1, "done.\n", 6);
+		exit (0);
+	}
+	else
+	{
+		write(1, "error.\n", 7);
+		exit (1);
+	}
+}
---
+++ server_bonus.c
@@ -0,0 +1,62 @@
+#include <unistd.h>
+#include <stdio.h>
+#include "minitalk.h"
+
+void	ft_write_sig(int signum)
+{
+	static char	chr = 0;
+	static char	bit_cnt = 0;
+
+	chr <<= 1;
+	if (signum == SIGUSR1)
+		chr = chr | 1;
+	else
+		chr = chr | 0;
+	++bit_cnt;
+	if (bit_cnt == 8)
+	{
+		write(1, &chr, 1);
+		if (chr == 0)
+			write(1, "\n", 1);
+		bit_cnt = 0;
+	}
+}
+
+void	ft_sigaction(int signum, siginfo_t *info, void *ucp)
+{
+	static char	chr = 0;
+	static char	bit_cnt = 0;
+	static char	first_check = 0;
+
+	chr <<= 1;
+	if (signum == SIGUSR1)
+		chr = chr | 1;
+	else
+		chr = chr | 0;
+	++bit_cnt;
+	if (bit_cnt == 8)
+	{
+		write(1, &chr, 1);
+		if (chr == 0)
+		{
+			kill(info->si_pid, SIGUSR1);
+			write(1, "\n", 1);
+		}
+		bit_cnt = 0;
+	}
+}
+
+int	main()
+{
+	struct sigaction s_sig;
+
+	ft_putstr_fd("server pid : ", 1);
+	ft_putnbr_fd(getpid(), 1);
+	ft_putchar_fd('\n', 1);
+	s_sig.sa_flags = SA_SIGINFO;
+	s_sig.sa_sigaction = ft_sigaction;
+	sigaction(SIGUSR1, &s_sig, NULL);
+	sigaction(SIGUSR2, &s_sig, NULL);
+	while (1)
+		pause();
+}
